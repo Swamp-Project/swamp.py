7cd7f6f165e02bdc25030b85734a8e0d0c637b9d
Mark Ditsworth <markditsworth@protonmail.com>
2019-10-17 15:52:04 -0500

    clean up the differentiation between cli and non cli usage

--- swamp.py
+++ swamp.py
@@ -13,55 +13,55 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 class Swamp(object):
 
-    def __init__(self, id=None, url=None, outfile=None):
-        self.gid = id
-        self.url = url
+    def __init__(self, cli=False, outfile=None): 
+        self.cli = cli
         self.outfile = outfile
 
-    def run(self, cli=False):
+    def run(self,id=None,url=None):
+        gid = id
         if self.outfile != None:
             # write date and time to file to initialize
             with open(self.outfile,'w') as fObj:
                 dt = datetime.utcnow().strftime("%Y-%m-%d %H:%M")
                 fObj.write("{}\n".format(dt))
 
-        if self.gid != None:
-            urls = self.scan_gid(self.gid, cli=cli)
-            if not cli:
+        if gid != None:
+            urls = self.scan_gid(gid)
+            if not self.cli:
                 return urls
 
-        elif self.url != None:
-            gids = self.get_gids_from_url(self.handle_url_protocol(self.url))
-            urls = self.scan_gids(gids, cli=cli)
-            if not cli:
+        elif url != None:
+            gids = self.get_gids_from_url(self.handle_url_protocol(url))
+            urls = self.scan_gids(gids)
+            if not self.cli:
                 return urls
 
         else:
-            print(Fore.RED + "You must pass in either '-url <webpage url>' or '-id <google tracking id>'")
-            print(Style.RESET_ALL)
-
-    def run_cli(self):
-        self.run(cli=True)
+            if self.cli:
+                print(Fore.RED + "You must pass in either '-url <webpage url>' or '-id <google tracking id>'")
+                print(Style.RESET_ALL)
+            else:
+                assert False, "You must pass in either url=<webpage url string> or id=<google tracking id string>"
 
     def show_banner(self):
-
-        print()
-        print(Fore.YELLOW + 
-        """
-        .d8888b.  888       888       d8888888b     d8888888888b.
-        d88P  Y88b888   o   888      d888888888b   d8888888   Y88b
-        Y88b.     888  d8b  888     d88P88888888b.d88888888    888
-        "Y888b.   888 d888b 888    d88P 888888Y88888P888888   d88P
-            "Y88b.888d88888b888   d88P  888888 Y888P 8888888888P"
-              "88888888P Y88888  d88P   888888  Y8P  888888
-        Y88b  d88P8888P   Y8888 d8888888888888   "   888888
-         "Y8888P" 888P     Y888d88P     888888       888888          
-        """)
+        if self.cli:
+            print()
+            print(Fore.YELLOW + 
+            """
+            .d8888b.  888       888       d8888888b     d8888888888b.
+            d88P  Y88b888   o   888      d888888888b   d8888888   Y88b
+            Y88b.     888  d8b  888     d88P88888888b.d88888888    888
+            "Y888b.   888 d888b 888    d88P 888888Y88888P888888   d88P
+                "Y88b.888d88888b888   d88P  888888 Y888P 8888888888P"
+                  "88888888P Y88888  d88P   888888  Y8P  888888
+            Y88b  d88P8888P   Y8888 d8888888888888   "   888888
+             "Y8888P" 888P     Y888d88P     888888       888888          
+            """)
     
-        print()
-        print(Fore.GREEN + "An OSINT tool for Google Analytics ID Reverse lookup")
-        print(Fore.RED + "By Jake Creps | With help from Francesco Poldi, WebBreacher and Mark Ditsworth")
-        print(Fore.WHITE)
+            print()
+            print(Fore.GREEN + "An OSINT tool for Google Analytics ID Reverse lookup")
+            print(Fore.RED + "By Jake Creps | With help from Francesco Poldi, WebBreacher and Mark Ditsworth")
+            print(Fore.WHITE)
 
     def handle_url_protocol(self,url):
         pattern = re.compile('^http[s]?\://')
@@ -73,7 +73,8 @@ class Swamp(object):
             else:
                 return validated_url
         else:
-            print(Fore.YELLOW + "Protocol not given. Will try HTTPS and then HTTP.")
+            if self.cli:
+                print(Fore.YELLOW + "Protocol not given. Will try HTTPS and then HTTP.")
             # test if https will work
             https_url = 'https://' + url
             validated_https_url = self.validate_url(https_url)
@@ -118,17 +119,17 @@ class Swamp(object):
             print(Fore.GREEN + "Discovered " + Fore.YELLOW + "{}".format(gid) + Fore.GREEN + " Google Tracking ID in " + Fore.WHITE + "{}".format(url))
         return gids_list
 
-    def scan_gids(self, ids, cli=True):
-        if cli:
+    def scan_gids(self, ids):
+        if self.cli:
             for _id in ids:
                 self.scan_gid(_id)
         else:
             urls = []
             for _id in ids:
-                urls.extend(self.scan_gid(_id,cli=False))
+                urls.extend(self.scan_gid(_id))
             return urls
 
-    def scan_gid(self, id, cli=True):
+    def scan_gid(self, id):
         
         print()
         print(Fore.GREEN + "Using {} for Google Analytic Lookup".format(id))
@@ -142,7 +143,7 @@ class Swamp(object):
             print(Fore.RED + "[ !!! ]   ERROR - {}".format(str(e)))
             sys.exit(1)
 
-        if cli:
+        if self.cli:
             print(Fore.YELLOW + "[+] " + Fore.RED + "Searching for associated URLs...")
 
         # Output is already JSON so we just need to load and parse it
@@ -155,7 +156,7 @@ class Swamp(object):
         for entry in j['results']:
             uniqueurls.add((entry['page']['url']))
         
-        if cli:
+        if self.cli:
             print(Fore.YELLOW + "[+] " + Fore.RED + "Outputting discovered URLs associate to {}...".format(id))
         
         if self.outfile != None:
@@ -164,14 +165,14 @@ class Swamp(object):
 
         # Sort the set and print
         for url in sorted(uniqueurls):
-            if cli:
+            if self.cli:
                 print(Fore.YELLOW + '[!]' + Fore.GREEN + " URL: " + Fore.WHITE + url)
             if self.outfile != None:
                 with open(self.outfile,'a') as fObj:
                     fObj.write("URL: {}\n".format(url))
-        if cli:
-            print(Style.RESET_ALL)
-        else:
+        
+        print(Style.RESET_ALL)
+        if not self.cli:
             return list(uniqueurls)
 
 if __name__ == '__main__':
@@ -181,6 +182,6 @@ if __name__ == '__main__':
     ap.add_argument('-o', help="Output file for results", action="store")
     args = ap.parse_args()
 
-    SwampApp = Swamp(id=args.id, url=args.url, outfile=args.o)
+    SwampApp = Swamp(cli=True,outfile=args.o)
     SwampApp.show_banner()
-    SwampApp.run_cli()
+    SwampApp.run(id=args.id,url=args.url)
