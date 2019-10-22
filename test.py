77557385e1f9fc3a65efe12066e54f8ca25b4a58
Mark Ditsworth <markditsworth@protonmail.com>
2019-10-22 16:26:38 -0500

    python module tests passed

--- test.py
+++ test.py
@@ -12,17 +12,21 @@ spyonweb_token = args.spyonweb_token
 print("Testing urlscan.io (default)")
 Swamp = swamp.Swamp()
 output = Swamp.run(id=test_id)
-assert len(output) > 0, "FAILED ON URLSCAN (default) WITH ID"
+print(output['urlscan'])
+assert len(output['urlscan']) > 0, "FAILED ON URLSCAN (default) WITH ID"
 output = Swamp.run(url=test_url)
-assert len(output) > 0, "FAILED ON URLSCAN (default) WITH URL"
+print(output['UA-6888464-2']['urlscan'])
+assert len(output['UA-6888464-2']['urlscan']) > 0, "FAILED ON URLSCAN (default) WITH URL"
 print("SUCCESS.\n\n")
 
 print("Testing urlscan.io (explicit call)")
 Swamp = swamp.Swamp(api="urlscan")
 output = Swamp.run(id=test_id)
-assert len(output) > 0, "FAILED ON URLSCAN (explicit) WITH ID"
+print(output['urlscan'])
+assert len(output['urlscan']) > 0, "FAILED ON URLSCAN (explicit) WITH ID"
 output = Swamp.run(url=test_url)
-assert len(output) > 0, "FAILED ON URLSCAN (explicit) WITH URL"
+print(output['UA-6888464-2']['urlscan'])
+assert len(output['UA-6888464-2']['urlscan']) > 0, "FAILED ON URLSCAN (explicit) WITH URL"
 print("SUCCESS.\n\n")
 
 if spyonweb_token == None:
@@ -31,8 +35,40 @@ else:
     print("Testing SpyOnWeb")
     Swamp = swamp.Swamp(api="spyonweb",token=spyonweb_token)
     output = Swamp.run(id=test_id)
-    assert len(output) > 0, "FAILED ON SPYONWEB WITH ID"
+    print(output['spyonweb'])
+    assert len(output['spyonweb']) > 0, "FAILED ON SPYONWEB WITH ID"
     output = Swamp.run(url=test_url)
-    assert len(output) > 0, "FAILED ON SPYONWEB WITH URL"
+    print(output['UA-6888464-2']['spyonweb'])
+    assert len(output['UA-6888464-2']['spyonweb']) > 0, "FAILED ON SPYONWEB WITH URL"
     print("SUCCESS.\n\n")
+
+    print("Testing Both through 'all'")
+    Swamp = swamp.Swamp(api="all",token=spyonweb_token)
+    output = Swamp.run(id=test_id)
+    print(output['spyonweb'])
+    assert len(output['spyonweb']) > 0, "FAILED ON SPYONWEB WITH ID"
+    print(output['urlscan'])
+    assert len(output['urlscan']) > 0, "FAILED ON URLSCAN WITH ID"
+    output = Swamp.run(url=test_url)
+    print(output['UA-6888464-2']['spyonweb'])
+    assert len(output['UA-6888464-2']['spyonweb']) > 0, "FAILED ON SPYONWEB WITH URL"
+    print(output['UA-6888464-2']['urlscan'])
+    assert len(output['UA-6888464-2']['urlscan']) > 0, "FAILED ON URLSCAN WITH URL"
+    print("SUCCESS.\n\n")
+
+    print("Testing Both through list")
+    Swamp = swamp.Swamp(api=['urlscan','spyonweb'],token=spyonweb_token)
+    output = Swamp.run(id=test_id)
+    print(output['spyonweb'])
+    assert len(output['spyonweb']) > 0, "FAILED ON SPYONWEB WITH ID"
+    print(output['urlscan'])
+    assert len(output['urlscan']) > 0, "FAILED ON URLSCAN WITH ID"
+    output = Swamp.run(url=test_url)
+    print(output['UA-6888464-2']['spyonweb'])
+    assert len(output['UA-6888464-2']['spyonweb']) > 0, "FAILED ON SPYONWEB WITH URL"
+    print(output['UA-6888464-2']['urlscan'])
+    assert len(output['UA-6888464-2']['urlscan']) > 0, "FAILED ON URLSCAN WITH URL"
+    print("SUCCESS.\n\n")
+
+
 
